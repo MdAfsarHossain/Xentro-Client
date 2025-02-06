8e966423fde14237ec9f23d3cc189dea44d43cbd
MdAfsarHossain <afsar291722@gmail.com>
2025-02-07 00:07:34 +0600

    Admin Menu JSX file updated

--- src/components/Sidebar/Menu/AdminMenu.jsx
+++ src/components/Sidebar/Menu/AdminMenu.jsx
@@ -1,12 +1,16 @@
 import { BsFillHouseAddFill, BsGraphUp } from 'react-icons/bs';
 import { FaUserCog } from 'react-icons/fa';
+import { MdOutlineManageHistory } from "react-icons/md";
 import MenuItem from './MenuItem';
+
 const AdminMenu = () => {
     return (
         <>
             <MenuItem icon={BsGraphUp} label='All Products' address='products' />
             <MenuItem icon={FaUserCog} label='All Users' address='users' />
             <MenuItem icon={BsFillHouseAddFill} label='Add Products' address='add-product'
+            />
+            <MenuItem icon={MdOutlineManageHistory} label='My Products' address='my-products'
             />
         </>
     )
